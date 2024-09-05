d3447b92f32d1564aa2799a833aa02ff655b0683
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2024-09-06 03:01:18 +0530

    camera feature added
    camera all feature added other than the load saved images

--- app/(tabs)/camera.jsx
+++ app/(tabs)/camera.jsx
@@ -1,7 +1,7 @@
 import { CameraView, useCameraPermissions } from 'expo-camera'
 import * as MediaLibrary from "expo-media-library"
-import React, { useState } from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import Colors from '../../constants/Colors'
 import Button from "./../../components/Camera/Button"
 
@@ -17,10 +17,17 @@ export default function Cam() {
     animateShutter : false,
     enableTorch : false
   })
+
+  const [image , setImage] = useState(null)
+
+  const [previousImage , setPreviousImage] = useState(null)
+  const cameraRef = useRef(null)
   
   if(!cameraPermission || !mediaLibraryPermission){
     return <View/>
   }
+
+ 
 
   if(!cameraPermission.granted || mediaLibraryPermission.status !=='granted'){
       return(
@@ -50,13 +57,68 @@ export default function Cam() {
   }
 
 
+  const takePicture = async()=>{
+      if(cameraRef.current){
+        try{
+          const picture = await cameraRef.current.takePictureAsync()
+          setImage(picture.uri)
+        }catch(err){
+          console.log("Error while taking picture" ,err)
+        }
+      }
+  }
+
+  const savePicture = async()=>{
+    if(image){
+      try{
+        const asset = await MediaLibrary.createAssetAsync(image)
+        const assetInfo = await MediaLibrary.getAssetInfoAsync(asset.id)
+        Alert.alert('Photo Saved!' , image)
+        setImage(null)
+        // getLastSavedImage()
+      }catch(err){
+        console.log("Error saving image" ,err)
+      }
+    }
+  }
+
+// const getLastSavedImage = async()=>{
+//   if(mediaLibraryPermissionResponse && mediaLibraryPermissionResponse.status==='granted'){
+//       const dcmi = await MediaLibrary.getAlbumAsync('DCIM')
+//       if(dcmi){
+//         const {assets} = await MediaLibrary.getAssetsAsync({
+//           album : dcmi,
+//           sortBy : [[MediaLibrary.SortBy.creationTime,false]],
+//           mediaType : MediaLibrary.MediaType.photo,
+//           first  : 1
+//         })
+
+//         if(assets.length>0){
+//           const assetInfo = await MediaLibrary.getAssetInfoAsync[assets[0].id]
+//           setPreviousImage(assetInfo.localUri || assetInfo.uri)
+//         }else{
+//           setPreviousImage(null)
+//         }
+//       }else{
+//         setPreviousImage(null)
+//       }
+//     }
+// }
+
+// useEffect(()=>{
+//   if(cameraPermission && cameraPermission.granted && mediaLibraryPermissionResponse && mediaLibraryPermissionResponse.status==='granted'){
+//     getLastSavedImage()
+//   }
+// },[cameraPermission, mediaLibraryPermissionResponse ])
+
+
   return (
     <View style={styles.container}>
-      <View style={styles.topControlsContainer}>
-        <Button icon='flip-camera-ios' 
-          onPress={()=>toggleProperty('facing', 'front','back')}
+      {
+        !image?(
+          <>
+              <View style={styles.topControlsContainer}>
         
-        />
         <Button icon={cameraProps.flash==='on'?'flash-on' : 'flash-off'}
           onPress={()=>toggleProperty('flash','on','off')}
         />
@@ -74,8 +136,32 @@ export default function Cam() {
         flash = {cameraProps.flash}
         animateShutter = {cameraProps.animateShutter}
         enableTorch = {cameraProps.enableTorch}
-      
+        ref={cameraRef}
       />
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity onPress={()=>previousImage && setImage(previousImage)}>
+
+        <Image source={{uri:previousImage}} style={styles.previousImage}/>
+        </TouchableOpacity>
+           
+            <Button icon='camera' size={60} style={{height:70}} onPress={takePicture}/>
+            <Button icon='flip-camera-ios' 
+          onPress={()=>toggleProperty('facing', 'front','back')}
+          size={30}
+        />
+      </View>
+          </>
+        ):(
+          <>
+            <Image source={{uri:image}} style = {styles.container}/>
+            <View style = {styles.bottomContainer}>
+              <Button  icon='flip-camera-android' size={60} style={{height:70}} onPress={()=>setImage(null)}/>
+              <Button icon='check' size={60} style={{height:70}} onPress={savePicture}/> 
+            </View>
+          </>
+        )
+      }
+      
       </View>
   )
 }
@@ -84,7 +170,7 @@ const styles = StyleSheet.create({
     container : {
     flex : 1,
     backgroundColor : '#fff',
-   
+    marginTop : 30
   },topControlsContainer : {
     height : 100,
     backgroundColor : 'black',
@@ -97,7 +183,7 @@ const styles = StyleSheet.create({
     backgroundColor : Colors.PRIMARY,
     padding : 10,
     margin : 10,
-    borderRadius : 9
+    borderRadius : 5
   },
   buttonText : {
     fontFamily : 'outfit-mid',
@@ -107,5 +193,16 @@ const styles = StyleSheet.create({
     flex : 1,
     width : '100%'
   },
-  
+  bottomContainer : {
+    height :70,
+    backgroundColor : 'black',
+    flexDirection : 'row',
+    justifyContent : 'space-around',
+    alignItems : 'center'
+  },
+  previousImage : {
+    width: 30,
+    height :30 ,
+    borderRadius : 50 
+  }
 })
