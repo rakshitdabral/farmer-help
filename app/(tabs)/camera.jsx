ae63cdeddd27d04b1f16b8f8636530c38bca1e37
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2024-09-07 19:35:27 +0530

    camera last saved image added
    added last saved image and zoom feature

--- app/(tabs)/camera.jsx
+++ app/(tabs)/camera.jsx
@@ -1,231 +1,267 @@
-import { CameraView, useCameraPermissions } from 'expo-camera'
-import * as MediaLibrary from "expo-media-library"
-import { useNavigation } from 'expo-router'
-import React, { useRef, useState } from 'react'
-import { Alert, Dimensions, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import Colors from '../../constants/Colors'
-import Button from "./../../components/Camera/Button"
+import Slider from '@react-native-community/slider';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import { useNavigation } from 'expo-router';
+import React, { useEffect, useRef, useState } from 'react';
+import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Button from './../../components/Camera/Button';
+export default function App() {
+    const navigation = useNavigation();
+    const [cameraPermission, requestCameraPermission] = useCameraPermissions();
+    const [mediaLibraryPermissionResponse, requestMediaLibraryPermission] = MediaLibrary.usePermissions();
+    const [cameraProps, setCameraProps] = useState({
+        zoom: 0,
+        facing: 'front',
+        flash: 'on',
+        animateShutter: false,
+        enableTorch: false
+    });
+    const [image, setImage] = useState(null);
+    const [previousImage, setPreviousImage] = useState(null);
 
-export default function Cam() {
-  const navigation = useNavigation();
-  const [cameraPermission, requestCameraPermission] = useCameraPermissions()
-  const [mediaLibraryPermission, requestMediaLibraryPermission] = MediaLibrary.usePermissions() 
+    const cameraRef = useRef(null);
 
-  const [cameraProps, setCameraProps] = useState({
-    zoom : 0,
-    facing : 'front',
-    flash : 'off',
-    animateShutter : false,
-    enableTorch : false
-  })
-
-  const [image , setImage] = useState(null)
-
-  const [previousImage , setPreviousImage] = useState(null)
-  const cameraRef = useRef(null)
-  
-  if(!cameraPermission || !mediaLibraryPermission){
-    return <View/>
-  }
-
- 
-
-  if(!cameraPermission.granted || mediaLibraryPermission.status !=='granted'){
-      return(
-        <View  style={styles.containerPermission}>
-          <Text style={{
-            fontFamily : 'outfit',
-            fontSize : 20
-          }}>We need your permission to continue</Text>
-          <TouchableOpacity style={styles.buttonPermission} 
-          
-          onPress={()=>{
-            requestCameraPermission()
-            requestMediaLibraryPermission()
-          }}
-          >
-            <Text style={styles.buttonText}>Grant Permission </Text>
-          </TouchableOpacity>
-        </View>
-      )
-  }
-
-
-  const toggleProperty  = (prop, option1,option2) =>{
-    setCameraProps((current)=>({
-      ...current , [prop]:current[prop] === option1 ? option2 : option1
-    }))
-  }
-
-
-  const takePicture = async()=>{
-      if(cameraRef.current){
-        try{
-          const picture = await cameraRef.current.takePictureAsync()
-          setImage(picture.uri)
-        }catch(err){
-          console.log("Error while taking picture" ,err)
+    //to load the last saved image when permissions change
+    useEffect(() => {
+        if(cameraPermission && cameraPermission.granted && mediaLibraryPermissionResponse && mediaLibraryPermissionResponse.status === 'granted') {
+            getLastSavedImage();
         }
-      }
-  }
+    }, [cameraPermission, mediaLibraryPermissionResponse])
 
-  const savePicture = async()=>{
-    if(image){
-      try{
-        const asset = await MediaLibrary.createAssetAsync(image)
-        const assetInfo = await MediaLibrary.getAssetInfoAsync(asset.id)
-        console.log(asset)
-        Alert.alert('Photo Saved!' , image)
-        
-        setImage(null)
-        console.log('Image before navigation:', image); // Add this line
-        navigation.push('disease', { image: image });
-        // getLastSavedImage()
-        // console.log(image)
-        
-      }catch(err){
-        console.log("Error saving image" ,err)
-      }
+    if (!cameraPermission || !mediaLibraryPermissionResponse) {
+        // Permissions are still loading.
+        return <View />
     }
+  
+    if (!cameraPermission.granted || mediaLibraryPermissionResponse.status !== 'granted') {
+        // Permissions are not granted yet.
+        return (
+          <View style={styles.container}>
+              <Text>We need camera and gallery permissions to continue.</Text>
+              <TouchableOpacity style={styles.button} onPress={() => {
+                  requestCameraPermission();
+                  requestMediaLibraryPermission();
+              }} >
+                  <Text style={styles.buttonText}>Grant Permissions</Text>
+              </TouchableOpacity>
+          </View>
+        )
+    }
+  
+    //function to toggle camera properties
+    const toggleProperty = (prop, option1, option2) => {
+        setCameraProps((current) => ({
+            ...current,
+            [prop]:current[prop] === option1 ? option2 : option1
+        }));
+    };
+
+    //function to zoom in
+    const zoomIn = () => {
+        setCameraProps((current) => ({
+            ...current,
+            zoom: Math.min(current.zoom + 0.1, 1)
+        }))
+    }
+
+    //function to zoom out
+    const zoomOut = () => {
+      setCameraProps((current) => ({
+          ...current,
+          zoom: Math.max(current.zoom - 0.1, 0)
+      }))
   }
 
-// const getLastSavedImage = async()=>{
-//   if(mediaLibraryPermissionResponse && mediaLibraryPermissionResponse.status==='granted'){
-//       const dcmi = await MediaLibrary.getAlbumAsync('DCIM')
-//       if(dcmi){
-//         const {assets} = await MediaLibrary.getAssetsAsync({
-//           album : dcmi,
-//           sortBy : [[MediaLibrary.SortBy.creationTime,false]],
-//           mediaType : MediaLibrary.MediaType.photo,
-//           first  : 1
-//         })
+  //function to take a picture and show it without saving it
+  const takePicture = async() => {
+      if(cameraRef.current) {
+          try {
+              const picture = await cameraRef.current.takePictureAsync();
+              setImage(picture.uri);
+          } catch (err) {
+            console.log('Error while taking the picture : ', err);
+          }
+      }
+  }
 
-//         if(assets.length>0){
-//           const assetInfo = await MediaLibrary.getAssetInfoAsync[assets[0].id]
-//           setPreviousImage(assetInfo.localUri || assetInfo.uri)
-//         }else{
-//           setPreviousImage(null)
-//         }
-//       }else{
-//         setPreviousImage(null)
-//       }
-//     }
-// }
+  //function to save the picture using MediaLibrary
+  const savePicture = async() => {
+      if(image) {
+          try {
+              const asset = await MediaLibrary.createAssetAsync(image);
+              const assetInfo = await MediaLibrary.getAssetInfoAsync(asset.id);
+              Alert.alert('Photo saved!', image);
+              setImage(null);
+              getLastSavedImage();
+              navigation.push('disease', { image: image });
+          } catch (err) {
+              console.log('Error while saving the picture : ', err);
+          }
+      }
+  }
 
-// useEffect(()=>{
-//   if(cameraPermission && cameraPermission.granted && mediaLibraryPermissionResponse && mediaLibraryPermissionResponse.status==='granted'){
-//     getLastSavedImage()
-//   }
-// },[cameraPermission, mediaLibraryPermissionResponse ])
+  //function to get the last saved image from the 'DCIM' album created in the gallery by expo
+  const getLastSavedImage = async() => {
+      if(mediaLibraryPermissionResponse && mediaLibraryPermissionResponse.status === 'granted') {
+          const dcimAlbum = await MediaLibrary.getAlbumAsync('DCIM');
 
-  
+          if(dcimAlbum) {
+              const {assets} = await MediaLibrary.getAssetsAsync({
+                  album: dcimAlbum,
+                  sortBy: [[MediaLibrary.SortBy.creationTime, false]],
+                  mediaType: MediaLibrary.MediaType.photo,
+                  first: 1
+              });
+
+              if(assets.length > 0) {
+                  const assetInfo = await MediaLibrary.getAssetInfoAsync(assets[0].id);
+                  setPreviousImage(assetInfo.localUri || assetInfo.uri);
+              } else {
+                  setPreviousImage(null);
+              }
+
+          } else {
+              setPreviousImage(null);
+          }
+      }
+  }
+
   return (
-    
     <View style={styles.container}>
-      {
-        !image?(
+      {!image ? (
           <>
               <View style={styles.topControlsContainer}>
-        
-        <Button icon={cameraProps.flash==='on'?'flash-on' : 'flash-off'}
-          onPress={()=>toggleProperty('flash','on','off')}
-        />
-        <Button icon='animation' 
-          color={cameraProps.animateShutter? 'white' : '#404040'}
-          onPress={()=>toggleProperty('animateShuttle',true,false)}
-        />
-        <Button icon={cameraProps.enableTorch?'flashlight-on' : 'flashlight-off'} 
-          onPress={()=>toggleProperty('enableTorch', true, false)}
-        />
-      </View>
-      <CameraView style={styles.camera}
-        zoom={cameraProps.zoom}
-        facing={cameraProps.facing}
-        flash = {cameraProps.flash}
-        animateShutter = {cameraProps.animateShutter}
-        enableTorch = {cameraProps.enableTorch}
-        ref={cameraRef}
-       
-      />
-      <View style={styles.bottomContainer}>
-        <TouchableOpacity onPress={()=>previousImage && setImage(previousImage)}>
-
-        <Image source={{uri:previousImage}} style={styles.previousImage}/>
-        </TouchableOpacity>
-           
-            <Button icon='camera' size={60} style={{height:70}} onPress={takePicture}/>
-            <Button icon='flip-camera-ios' 
-          onPress={()=>toggleProperty('facing', 'front','back')}
-          size={30}
-        />
-      </View>
-          </>
-        ):(
-          <>
-            <Image source={{uri:image}} style = {styles.container}/>
-            <View style = {styles.bottomContainer}>
-              <Button  icon='flip-camera-android' size={60} style={{height:70}} onPress={()=>setImage(null)}/>
-              <Button icon='check' size={60} style={{height:70}} onPress={savePicture}/> 
+              
+              <Button 
+                  icon={cameraProps.flash === 'on' ? 'flash-on' : 'flash-off'}
+                  onPress={() => toggleProperty('flash', 'on', 'off')}
+              />
+              <Button 
+                  icon='animation'
+                  color={cameraProps.animateShutter ? 'white' : '#404040'}
+                  onPress={() => toggleProperty('animateShutter', true, false)}
+              />
+              <Button 
+                  icon={cameraProps.enableTorch ? 'flashlight-on' : 'flashlight-off'}
+                  onPress={() => toggleProperty('enableTorch', true, false)}
+              />
+            </View>
+            <CameraView 
+                style={styles.camera} 
+                zoom={cameraProps.zoom}
+                facing={cameraProps.facing}
+                flash={cameraProps.flash}
+                animateShutter={cameraProps.animateShutter}
+                enableTorch={cameraProps.enableTorch}
+                ref={cameraRef}
+            />
+            <View style={styles.sliderContainer}>
+              <Button 
+                  icon='zoom-out'
+                  onPress={zoomOut}
+              />
+              <Slider 
+                  style= {styles.slider}
+                  minimumValue={0}
+                  maximumValue={1}
+                  value={cameraProps.zoom}
+                  onValueChange={(value) => setCameraProps((current) => ({...current, zoom:value}))}
+                  step={0.1}
+              />
+              <Button 
+                  icon='zoom-in'
+                  onPress={zoomIn}
+              />
+            </View>
+            <View style={styles.bottomControlsContainer}> 
+                <TouchableOpacity onPress={() => previousImage && setImage(previousImage)}>
+                    <Image 
+                        source={{uri:previousImage}}
+                        style={styles.previousImage}
+                    />
+                </TouchableOpacity>
+                
+                <Button 
+                    icon='camera'
+                    size={60}
+                    style={{height:60}}
+                    onPress={takePicture}
+                />
+                <Button 
+                  icon='flip-camera-ios'
+                  onPress={() => toggleProperty('facing', 'front', 'back')}
+                  size={40}
+              />
             </View>
           </>
-        )
-      }
+      ) : (
+          <>
+              <Image source={{uri:image}} style={styles.camera}/>
+              <View style={styles.bottomControlsContainer}>
+                  <Button 
+                      icon='flip-camera-android'
+                      onPress={()=> setImage(null)}
+                  />
+                  <Button 
+                      icon='check'
+                      onPress={savePicture}
+                  />
+              </View>
+          </>
+      )}
       
-      </View>
-  )
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container : {
-    flex : 1,
-    backgroundColor : '#fff',
-    
-  },topControlsContainer : {
-    height : 90,
-    backgroundColor : 'black',
-    flexDirection : 'row',
-    justifyContent: 'space-around',
-    alignItems : 'center',
-    
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 30,
   },
-  button : {
-    backgroundColor : Colors.PRIMARY,
-    padding : 10,
-    margin : 10,
-    borderRadius : 5
+  topControlsContainer: {
+    height: 100,
+    backgroundColor:'black',
+    flexDirection: 'row',
+    justifyContent:'space-around',
+    alignItems: 'center'
   },
-  buttonText : {
-    fontFamily : 'outfit-mid',
-    fontSize : 16
-  },
-  camera : {
-    flex : 1,
-    width : '100%'
-  },
-  bottomContainer : {
-    height :70,
-    backgroundColor : 'black',
-    flexDirection : 'row',
-    justifyContent : 'space-around',
-    alignItems : 'center'
-  },
-  previousImage : {
-    width: 30,
-    height :30 ,
-    borderRadius : 50 
-  },
-  containerPermission : {
-    flex : 1,
-    justifyContent : 'center',
-    alignItems : 'center'
-  },
-  buttonPermission:{
-    backgroundColor : Colors.PRIMARY,
-    padding : 10,
-    margin : 10,
-    borderRadius : 5,
-    justifyContent : 'center',
-    alignItems : 'center',
-    width : Dimensions.get('window').width*0.6
-  }
-})
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+},
+buttonText: {
+    color: 'white',
+    fontSize: 16,
+},
+camera: {
+  flex:1,
+  width: '100%',
+},
+slider: {
+  flex:1,
+  marginHorizontal: 10,
+},
+sliderContainer: {
+  position: 'absolute',
+  bottom: 120,
+  left : 20,
+  right: 20,
+  flexDirection: 'row'
+},
+bottomControlsContainer: {
+  height:100,
+  backgroundColor: 'black',
+  flexDirection: 'row',
+  justifyContent:'space-around',
+  alignItems:'center'
+},
+previousImage: {
+  width:60,
+  height:60,
+  borderRadius: 50
+}
+});
