0dc68be96c78c7f8447c581a0ec8799320b3b271
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2024-09-08 19:17:42 +0530

    added animation and text title
    added title for container and animation

--- components/Home/Realtimedata.jsx
+++ components/Home/Realtimedata.jsx
@@ -1,6 +1,6 @@
 import { onValue, ref } from 'firebase/database';
-import React, { useEffect, useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { Animated, Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { realTimeDatabase } from '../../config/FirebaseConfig';
 import Colors from "../../constants/Colors";
 
@@ -10,12 +10,38 @@ export default function Realtimedata() {
   const [pressure, setPressure] = useState(null);
   const [altitude, setAltitude] = useState(null);
   const [humidity, setHumidity] = useState(null);
+  const animatedValue = useRef(new Animated.Value(1)).current;
 
-  useEffect(() => { 
+  const [dataVisibility, setDataVisibility] = useState({
+    temperature:true,
+    soilMoisture:true,
+    pressure:true,
+    altitude:true,
+    humidity:true,
+  }) 
+
+  // animation for icons
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(animatedValue, {
+          toValue: 1.5, 
+          duration: 1000, 
+          useNativeDriver: true,
+        }),
+        Animated.timing(animatedValue, {
+          toValue: 1, 
+          duration: 1000, 
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }, [animatedValue]);
+
+  useEffect(() => {
     const dataRef = ref(realTimeDatabase, '/data'); 
     const unsubscribe = onValue(dataRef, (snapshot) => {
       const fetchedData = snapshot.val();
-      // console.log('Data from Firebase:', fetchedData);
 
       if (fetchedData) {
         const records = Object.values(fetchedData);
@@ -54,6 +80,14 @@ export default function Realtimedata() {
     return () => unsubscribe();
   }, []);
 
+  // Toggle visibility on touch
+  const handlepress = (key) => {
+    setDataVisibility((prevVisibility) => ({
+      ...prevVisibility,
+      [key]: !prevVisibility[key],
+    }));
+  };
+
   return (
     <View style={{ marginTop: 20 }}>
       <Text style={{ fontFamily: "outfit-mid", fontSize: 20 }}>
@@ -67,48 +101,78 @@ export default function Realtimedata() {
           flexWrap: "wrap",
           gap: 30,
           alignItems: "flex-start",
-          marginTop: 10
+          marginTop: 10,
         }}
       >
-        <View style={styles.container}>
-          <Image
-            source={require("./../../assets/images/temperature-icon.png")}
-            style={{ width: 40, height: 40 }}
-          />
-          <Text style={styles.text}>{temperature || 'Loading...'}</Text>
-        </View>
+        <TouchableOpacity onPress={() => handlepress('temperature')} style={{ width: "100%" }}>
+          <View style={styles.container}>
+            <Animated.Image
+              source={require("./../../assets/images/temperature-icon.png")}
+              style={[styles.icon, { transform: [{ scale: animatedValue }] }]}
+            />
+            {dataVisibility.temperature && (
+              <Text style={styles.text}>
+                {temperature ? `Temperature: ${temperature}` : 'Temperature'}
+              </Text>
+            )}
+          </View>
+        </TouchableOpacity>
 
-        <View style={styles.container}>
-          <Image
-            source={require("./../../assets/images/soil-moisture-icon.png")}
-            style={{ width: 40, height: 40 }}
-          />
-          <Text style={styles.text}>{soilMoisture || 'Loading...'}</Text>
-        </View>
+        <TouchableOpacity onPress={() => handlepress('soilMoisture')} style={{ width: "100%" }}>
+          <View style={styles.container}>
+            <Animated.Image
+              source={require("./../../assets/images/soil-moisture-icon.png")}
+              style={[styles.icon, { transform: [{ scale: animatedValue }] }]}
+            />
+            {dataVisibility.soilMoisture && (
+              <Text style={styles.text}>
+                {soilMoisture ? `Soil Moisture: ${soilMoisture}` : 'Soil Moisture'}
+              </Text>
+            )}
+          </View>
+        </TouchableOpacity>
 
-        <View style={styles.container}>
-          <Image
-            source={require("./../../assets/images/pressure-icon.png")}
-            style={{ width: 40, height: 40 }}
-          />
-          <Text style={styles.text}>{pressure || 'Loading...'} </Text>
-        </View>
+        <TouchableOpacity onPress={() => handlepress('pressure')} style={{ width: "100%" }}>
+          <View style={styles.container}>
+            <Animated.Image
+              source={require("./../../assets/images/pressure-icon.png")}
+              style={[styles.icon, { transform: [{ scale: animatedValue }] }]}
+            />
+            {dataVisibility.pressure && (
+              <Text style={styles.text}>
+                {pressure ? `Pressure: ${pressure}` : 'Pressure'}
+              </Text>
+            )}
+          </View>
+        </TouchableOpacity>
 
-        <View style={styles.container}>
-          <Image
-            source={require("./../../assets/images/altitude-icon.png")}
-            style={{ width: 40, height: 40 }}
-          />
-          <Text style={styles.text}>{altitude || 'Loading...'} </Text>
-        </View>
+        <TouchableOpacity onPress={() => handlepress('altitude')} style={{ width: "100%" }}>
+          <View style={styles.container}>
+            <Animated.Image
+              source={require("./../../assets/images/altitude-icon.png")}
+              style={[styles.icon, { transform: [{ scale: animatedValue }] }]}
+            />
+            {dataVisibility.altitude && (
+              <Text style={styles.text}>
+                {altitude ? `Altitude: ${altitude}` : 'Altitude'}
+              </Text>
+            )}
+          </View>
+        </TouchableOpacity>
 
-        <View style={styles.container}>
-          <Image
-            source={require("./../../assets/images/humidity-icon.png")}
-            style={{ width: 40, height: 40 }}
-          />
-          <Text style={styles.text}>{humidity || 'Loading...'}</Text>
-        </View>
+        <TouchableOpacity onPress={() => handlepress('humidity')} style={{ width: "100%" }}>
+          <View style={styles.container}>
+            <Animated.Image
+              source={require("./../../assets/images/humidity-icon.png")}
+              style={[styles.icon, { transform: [{ scale: animatedValue }] }]}
+            />
+            {dataVisibility.humidity && (
+              <Text style={styles.text}>
+                {humidity ? `Humidity: ${humidity}` : 'Humidity'}
+              </Text>
+            )}
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -123,7 +187,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     gap: 10,
-    padding: 8,
+    padding: 16,
     borderWidth: 1,
     borderColor: Colors.PRIMARY,
   },
@@ -131,5 +195,9 @@ const styles = StyleSheet.create({
     fontFamily: "outfit-mid",
     fontSize: 20,
     color: Colors.GRAY,
+  },
+  icon: {
+    width: 40,
+    height: 40,
   },
 });
